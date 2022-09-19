9c5f876177f312638b90948fd27b7ad84d973d86
KerriAnn1 <dragonsfire20@hotmail.com>
2022-09-19 15:55:32 -0400

    generate markdown

---
+++ generateMarkdown.js
@@ -0,0 +1,26 @@
+//to do; create a function that returns a liscence badge based on which liscence is passed in
+//if there is no lisence, return an empty string
+function renderLicenseBadge(license){
+
+}
+
+//to do create a function that returns the license link
+//if there is no lisence, return an empty string
+function renderLicenseLink(license){
+
+}
+
+
+//to do create a function that returns the license section of README
+//if there is no lisence, return an empty string
+function renderLicenseSection(license){
+
+}
+
+//to do create a function to generate markdown to readme
+function generateMarkdown(data){
+   console.log(data)
+    return `
+    # ${data.title}
+    `
+}
