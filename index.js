dfc4ff00d56e5cdbbe4d58338052ac23c160ba8a
KerriAnn1 <dragonsfire20@hotmail.com>
2022-09-19 15:57:02 -0400

    packages for application

---
+++ index.js
@@ -0,0 +1,5 @@
+//to do; include packages needed for this application
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
